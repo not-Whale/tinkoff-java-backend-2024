fa5a22cb3375c86bc0e124a4a7f6f5a405c586fd
not-Whale <rezepinn@yandex.ru>
2024-03-01 21:19:47 +0400

    Добавил ключ конфигурации

--- bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+// TODO: явно определить MarkupProcessor
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record ApplicationConfig(@NotEmpty String telegramToken) {}
+public record ApplicationConfig(@NotEmpty String telegramToken, @NotEmpty String languageCode) {}
