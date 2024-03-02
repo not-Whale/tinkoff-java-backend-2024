34b40dc297f5216b343cdba7e97ab9661303a8be
not-Whale <rezepinn@yandex.ru>
2024-03-02 15:40:59 +0400

    Добавил html форматирование, сделал явное указание выбора форматирования в конфигурации

--- bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
@@ -1,10 +1,17 @@
 package edu.java.bot.configuration;
 
+import edu.java.bot.response_creator.markup_processors.MarkdownProcessor;
+import edu.java.bot.response_creator.markup_processors.MarkupProcessor;
 import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
 
-// TODO: явно определить MarkupProcessor
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record ApplicationConfig(@NotEmpty String telegramToken, @NotEmpty String languageCode) {}
+public record ApplicationConfig(@NotEmpty String telegramToken, @NotEmpty String languageCode) {
+    @Bean
+    public MarkupProcessor markupProcessor() {
+        return new MarkdownProcessor();
+    }
+}
